063fc0e8a2f2d6789fb81015d118f5d4984f2590
John Godwin <winsheecodes@gmail.com>
2025-07-04 21:23:28 +0800

    Updated UI for Form, QR Generator, and QR Scanner

--- qrapp/forms.py
+++ qrapp/forms.py
@@ -5,3 +5,30 @@ class DriverInfoForm(forms.ModelForm):
     class Meta:
         model = DriverInfo
         fields = '__all__'
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
+                'placeholder': 'Enter first name'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
+                'placeholder': 'Enter last name'
+            }),
+            'contact_number': forms.TextInput(attrs={
+                'class': 'w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
+                'placeholder': 'Enter contact number'
+            }),
+            'address': forms.Textarea(attrs={
+                'class': 'w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
+                'placeholder': 'Enter address',
+                'rows': 3
+            }),
+            'license_number': forms.TextInput(attrs={
+                'class': 'w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
+                'placeholder': 'Enter license number'
+            }),
+            'vehicle_plate': forms.TextInput(attrs={
+                'class': 'w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
+                'placeholder': 'Enter vehicle plate number'
+            }),
+        }
