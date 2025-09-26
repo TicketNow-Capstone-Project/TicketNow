cb9ca0d7eba940a694dcc410afa369ba45fcf650
KleinLavina <kleinlav7@gmail.com>
2025-09-26 13:17:22 +0800

    partial design

--- ticket_models/views.py
+++ ticket_models/views.py
@@ -7,28 +7,8 @@ def ticket_page(request):
     return render(request, "ticket_models/base.html")  # app_name/template.html
 
 def page_view(request, page_name):
-    if page_name == "op1":
+    if page_name == "trips":
         trips = Trip_Table.objects.all()
-        return render(request, "ticket_models/all_trips.html", {"trips": trips})
-    elif page_name == "menu":   
-        return render(request, "ticket_models/menu.html")
-    elif page_name == "op2":   
-        tickets = Ticket_Table.objects.all()
-        return render(request, "ticket_models/all_tickets.html", {"tickets": tickets})
-    elif page_name == "op3":
-        if request.method == "POST":
-            form = TicketForm(request.POST)
-            if form.is_valid():
-                form.save()
-                messages.success(request, "🎟️ Ticket created successfully!")
-                form = TicketForm()  # reset form
-            else:
-                messages.error(request, "❌ Please correct the errors below.")
-        else:
-            form = TicketForm()
-        return render(request, "ticket_models/create_tickets.html", {"form": form})
-   
-    elif page_name == "op4":
         if request.method == "POST":
             form = TripForm(request.POST)
             if form.is_valid():
@@ -39,8 +19,24 @@ def page_view(request, page_name):
                 messages.error(request, "❌ Please correct the errors below.")  # error case
         else:
             form = TripForm()
-
-        return render(request, "ticket_models/create_trips.html", {"form": form})
+        return render(request, "ticket_models/all_trips.html", {"trips": trips, "form": form})
+    
+    elif page_name == "menu":   
+        return render(request, "ticket_models/menu.html")
+    elif page_name == "tickets":   
+        tickets = Ticket_Table.objects.all()
+        if request.method == "POST":
+                form = TicketForm(request.POST)
+                if form.is_valid():
+                    form.save()
+                    messages.success(request, "🎟️ Ticket created successfully!")
+                    form = TicketForm()  # reset form
+                else:
+                    messages.error(request, "❌ Please correct the errors below.")
+        else:
+            form = TicketForm()
+        return render(request, "ticket_models/all_tickets.html", {"tickets": tickets, "form": form})
+   
 
 
     else:
